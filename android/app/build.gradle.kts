2a016ef0b7cad005cb11282baf923d827cf9522a
Ashitosharcitech <ashitosh@arcitech.ai>
2025-05-21 17:21:35 +0530

    change scroll to bottom

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,14 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.chat_bot_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
- 
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
